b0e651e5084a036ccfd58bd54bbc0ca7d169c070
Artis <melnapanda@gmail.com>
2022-05-03 17:36:51 +0300

    update file upload file

--- fastApi.py
+++ fastApi.py
@@ -1,29 +1,19 @@
-# import uuid
-# import shutil
-from fastapi.responses import FileResponse
-from fastapi import FastAPI, File, Query, Response, UploadFile
-
-# import requests
+import shutil
+from fastapi import FastAPI, File,Query,  UploadFile
 
 app = FastAPI()
 
-@app.post("/upload_video")
-async def add_file(
+# File uploading
+@app.post("/upload_file")
+async def upload_file(
     session_id: str = Query(...),
     file: UploadFile = File(...)
-    ):
+):
+    extension = file.filename.split(".")[1]
+    file_name = session_id
+    file_location = f"unprocessedFiles/{file_name}.{extension}"
 
-# File saving in folder (don't need that, ja pareizi sapratu)
-    # with open(f'{file.filename}', "wb") as buffer:
-    #     shutil.copyfileobj(file.file, buffer)
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)    
+    return {"info": f"file '{file_name}' saved at '{file_location}'"}
 
-    return {file.filename: "uploaded successfully!"}
-
-# so far don't know how to get file by session_id, cause it's defined only in @post method
-@app.get("/get_file")
-async def file():
-    # need to get file by session_id:
-    return FileResponse({"filename.mp4"}) 
-
-# pēc tam varēs implementēt OpenCV
-# labāk būtu rakstīt visu OpenCV kodu tepat vai tomēr jaunā failā? (for cleaner code purposes, labāk laikam būtu jaunā failā)
