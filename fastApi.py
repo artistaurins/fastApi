710866bca9896dc620c20a55f611285f60096605
Artis <melnapanda@gmail.com>
2022-05-05 23:25:01 +0300

    add head_pose function to file upload

--- fastApi.py
+++ fastApi.py
@@ -1,5 +1,9 @@
 import shutil
+import uuid
+import aiofiles
 from fastapi import FastAPI, File,Query,  UploadFile
+
+from testing import head_pose
 
 app = FastAPI()
 
@@ -15,5 +19,19 @@ async def upload_file(
 
     with open(file_location, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)    
-    return {"info": f"file '{file_name}' saved at '{file_location}'"}
+
+    result = None
+    try:
+        file_contents = await file.read()
+        path_file = f'/unprocessedFiles/{uuid.uuid4()}.wav'
+        async with aiofiles.open(path_file, 'wb') as out_file:
+            await out_file.write(file_contents)
+        # process file or file_contents bytes
+        head_pose(file_name)
+
+    #     result = await ControllerRequests.do_something(request=message_request_voiceid, file=file)
+    except Exception as e:
+        # LoggingUtils.exception_log(e)
+    # return Response(content=result.to_json(), media_type="application/json")
+        return {"info": f"file '{file_name}' saved at '{file_location}'"}
 
---
+++ testing.py
@@ -0,0 +1,168 @@
+# First unfinished version
+
+# import cv2
+# # from cv2 import threshold
+# import numpy as np
+
+# file_location =f"unprocessedFiles/test3.mp4"
+# cap = cv2.VideoCapture(file_location)
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# while True:
+#     ret, frame = cap.read()
+#     # roi = frame[100: 1000, 50: 800]
+#     gray_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+
+#     # _, threshold = cv2.threshold(gray_roi, 5, 255, cv2.THRESH_BINARY_INV)
+
+#     # cv2.imshow("Threshhold", threshold)
+#     cv2.imshow("Roi", gray_roi)
+#     key = cv2.waitKey(30)
+#     if key == 27:
+#         break
+
+# cv2.destroyAllWindows
+
+
+##########################################################################################################
+
+# Second face and eye detection
+
+# import numpy as np
+# import cv2
+
+# file_location =f"unprocessedFiles/test3.mp4"
+# cap = cv2.VideoCapture(file_location)
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# while True:
+#     ret, frame = cap.read()
+
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+#         roi_gray = gray[y:y+w, x:x+w]
+#         roi_color = frame[y:y+h, x:x+w]
+#         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+#         for (ex, ey, ew, eh) in eyes:
+#             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+#     cv2.imshow('frame', frame)
+
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+# ########################################################################################################
+
+# Head pose estimation, but its working poorly
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+def head_pose(file_name):
+
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    file_location =f"unprocessedFiles/{file_name}.mp4"
+    cap = cv2.VideoCapture(file_location)
+
+    while cap.isOpened():
+        success, image = cap.read()
+
+        # Flip the image horizontally for a later selfie-view display
+        # Also convert the color space from BGR to RGB
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+        # To improve performance
+        image.flags.writeable = False
+        
+        # Get the result
+        results = face_mesh.process(image)
+        
+        # To improve performance
+        image.flags.writeable = True
+        
+        # Convert the color space from RGB to BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        img_h, img_w, img_c = image.shape
+        face_3d = []
+        face_2d = []
+
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                for idx, lm in enumerate(face_landmarks.landmark):
+                    if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                        if idx == 1:
+                            nose_2d = (lm.x * img_w, lm.y * img_h)
+                            nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000)
+
+                        x, y = int(lm.x * img_w), int(lm.y * img_h)
+
+                        # Get the 2D Coordinates
+                        face_2d.append([x, y])
+
+                        # Get the 3D Coordinates
+                        face_3d.append([x, y, lm.z])       
+                
+                # Convert it to the NumPy array
+                face_2d = np.array(face_2d, dtype=np.float64)
+
+                # Convert it to the NumPy array
+                face_3d = np.array(face_3d, dtype=np.float64)
+
+                # The camera matrix
+                focal_length = 1 * img_w
+
+                cam_matrix = np.array([ [focal_length, 0, img_h / 2],
+                                        [0, focal_length, img_w / 2],
+                                        [0, 0, 1]])
+
+                # The Distance Matrix
+                dist_matrix = np.zeros((4, 1), dtype=np.float64)
+
+                # Solve PnP
+                success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+                # Get rotational matrix
+                rmat, jac = cv2.Rodrigues(rot_vec)
+
+                # Get angles
+                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+                # Get the y rotation degree
+                x = angles[0] * 360
+                y = angles[1] * 360
+
+                if y < -10:
+                    text = "Looking Left"
+                elif y > 10:
+                    text = "Looking Right"
+                elif x < -10:
+                    text = "Looking Down"
+                else:
+                    text = "Forward"
+
+                nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
+
+                p1 = (int(nose_2d[0]), int(nose_2d[1]))
+                p2 = (int(nose_3d_projection[0][0][0]), int(nose_3d_projection[0][0][1]))
+                
+                cv2.line(image, p1, p2, (255, 0, 0), 2)
+
+                cv2.putText(image, text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        cv2.imshow('Head Pose Estimation', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+    cap.release()
